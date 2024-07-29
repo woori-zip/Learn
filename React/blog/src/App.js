a14cd46f27217f15f0f60aa01798917d9044bf00
woori <woori.dec@gmail.com>
2024-07-29 09:03:17 +0900

    JSX 문법 기초

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // 서버에서 데이터를 가져왔다고 가정하고 진행
+  let post = '여름 냄새나는 애니메이션 추천';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 1. class (X) className(O) */}
+      <div className="pink-nav">
+        {/* 3. Style 넣을 땐, { {스타일명:'값', ...} } */}
+        <h4 style={ {fontSize: '16px', fontWeight: 'bold' } }>Blog</h4>
+      </div>
+      {/* 2. 데이터바인딩은 { 중괄호 } */}
+      <h4>{ post }</h4>
     </div>
   );
 }
