579cb5f0a3012f85492d7ba86fb0721c0237108b
woori <woori.dec@gmail.com>
2024-07-31 12:50:03 +0900

    input, array 조작(unshift, splice)

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 
 function App() {
 
@@ -10,6 +10,8 @@ function App() {
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
+  let [날짜, 날짜변경] = useState(['2024-07-29','2024-07-29','2024-07-29']);
 
   function 따봉누름(i){
     let copy = [...따봉];
@@ -50,18 +52,51 @@ function App() {
                 setTitle(i); 
               }}>
                 { a }
+                {/* 이벤트 버블링을 막으려면 e.stopPropagation() */}
+                <span onClick={ (e) => { e.stopPropagation(); 따봉누름(i) } }>👍</span> { 따봉[i] }
               </h4>
-              <span onClick={ () => { 따봉누름(i) } }>👍</span> { 따봉[i] }
-              <p>7월 29일 발행</p>
-
+              <p>{날짜[i]} 발행</p>
+              <button onClick={()=>{
+                let 글제목copy = [...글제목];
+                글제목copy.splice(i, 1); // i번째 항목 제거
+                글제목변경(글제목copy);
+                let 날짜copy = [...날짜];
+                날짜copy.splice(i, 1);
+                날짜변경(날짜copy);
+                let 따봉copy=[...따봉];
+                따봉copy.splice(i, 1);
+                따봉변경(따봉copy);
+              }}>삭제</button>
             </div>
           )
         })
       }
+      
+      <input
+        value={입력값}
+        onChange={(e)=>{ 입력값변경(e.target.value); console.log(입력값) }}
+      />
+      <button onClick={()=>{
+        if(입력값!=='') {
+          let 글제목copy = [...글제목];
+          글제목copy.unshift(입력값);
+          글제목변경(글제목copy);
+          let 오늘날짜 = new Date().toISOString().split('T')[0];
+          let 날짜copy = [...날짜];
+          날짜copy.unshift(오늘날짜);
+          console.log(날짜copy);
+          날짜변경(날짜copy);
+          let 따봉copy = [0, ...따봉];
+          따봉변경(따봉copy);
+          입력값변경(''); // 입력 필드 초기화
+        }
+      }}>작성</button>
 
       {
-        modal == true ? <Modal title={title} 글제목={글제목} 제목바꿈={제목바꿈}/> : null
-      }   
+        modal == true 
+        ? <Modal title={title} 글제목={글제목} 날짜={날짜} 제목바꿈={제목바꿈}/> 
+        : null
+      }
 
     </div>
   );
@@ -72,7 +107,7 @@ function Modal(props) {
   return (
     <div className='modal' style={{background : props.color}}>
       <h4>{props.글제목[props.title]}</h4>
-      <p>날짜</p>
+      <p>{props.날짜[props.title]}</p>
       <p>상세내용</p>
       <button onClick={()=>{props.제목바꿈()}}>글수정</button>
     </div>
