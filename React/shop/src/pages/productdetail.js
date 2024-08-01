bc8e4a91569d795051670524107d3d8868cf7493
woori <woori.dec@gmail.com>
2024-08-01 16:37:51 +0900

    styled-components

--- React/shop/src/pages/productdetail.js
+++ React/shop/src/pages/productdetail.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+let ColorBtn = styled.button`
+  background : ${ props => props.bg };
+  border : none;
+  border-radius : 10px;
+  color : ${ props => props.bg == 'skyblue' ? 'black' : 'white'};
+  padding : 10px;
+`
 
 function ProductDetail(props) {
 
@@ -15,10 +24,11 @@ function ProductDetail(props) {
         <img src={찾은상품.img} width='80%' alt={찾은상품.title} />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{찾은상품.title}</h4>
+          <h5 className="pt-5">{찾은상품.title}</h5>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</p>
-          <button className="btn btn-danger">주문하기</button>
+          <ColorBtn bg='pink'>장바구니</ColorBtn>
+          <ColorBtn bg='skyblue'>주문하기</ColorBtn>
         </div>
       </div>
     </div>
