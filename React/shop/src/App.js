1f007b8e065b95cc692e8be9c72cf57a8e9a7d46
woori <woori.dec@gmail.com>
2024-08-01 13:43:56 +0900

    컴포넌트 + map

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -1,12 +1,77 @@
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
+import bannerImage from './img/bg.jpeg';
+import {useState} from 'react';
+import data from './data.js';
 
 function App() {
+
+  let [anime] = useState(data);
+  console.log(anime);
+
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      {/* navbar */}
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">スキップとローファー</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      {/* banner */}
+      <div className='main-bg' style={{backgroundImage : 'url(' + bannerImage + ')'}}></div>
+
+      <Container>
+      <Row>
+
+        {/* 컴포넌트만 */}
+        {/* <Card anime={anime[0]}/>
+        <Card anime={anime[1]}/>
+        <Card anime={anime[2]}/> */}
+
+        {/* map만 */}
+        {/* {
+          anime.map(function(anime,i){
+            return (
+              <Col key={i}>
+                <img src={anime.img} width='80%' alt={anime.title} />
+                <h5>{anime.title}</h5>
+                <p>{anime.content}</p>
+              </Col>
+            )
+          })
+        } */}
+
+        {/* 컴포넌트 + map */}
+        {
+          anime.map(function(anime,i){
+            return (
+              <Card anime={anime}/>
+            )
+          })
+        }
+      </Row>
+    </Container>
     </div>
   );
+}
+
+function Card(props) {
+  return (
+    <Col>
+      <img src={props.anime.img} width='80%' alt={props.anime.title} />
+      <h5>{props.anime.title}</h5>
+      <p>{props.anime.content}</p>
+    </Col>
+  )
 }
 
 export default App;
---
+++ React/shop/src/data.js
@@ -0,0 +1,32 @@
+let data = [
+  {
+    id:0
+    ,img:'https://i.pinimg.com/564x/79/9d/2f/799d2f6b16fd8164688c7473772c1acc.jpg'
+    ,title:'시마 소스케'
+    ,content:'츠바메니시 고등학교'
+    ,price:100900
+  },
+  {
+    id:1
+    ,img:'https://i.pinimg.com/736x/a8/7a/77/a87a7789e3c679804783a9f96909c301.jpg'
+    ,title:'이와쿠라 미츠미'
+    ,content:'츠바메니시 고등학교'
+    ,price:30300
+  },
+  {
+    id:2
+    ,img:'https://i.pinimg.com/736x/6c/b3/37/6cb337497f3201f48e4a5bbb48f34fb0.jpg'
+    ,title:'에가시라 미카'
+    ,content:'츠바메니시 고등학교'
+    ,price:72900
+  },
+  {
+    id:3
+    ,img: 'https://i.pinimg.com/564x/b3/c0/56/b3c056f3104d934ab99a613d4e9a0dc6.jpg'
+    ,title:'무라시게 유즈키'
+    ,content:'츠바메니시 고등학교'
+    ,price:121100
+  }
+]
+
+export default data;
